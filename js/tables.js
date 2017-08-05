5057d9fca9e86bc6a3afb7a293c208ac2680ebc3
ruthbuena <ruthbpeace@gmail.com>
2017-08-05 11:11:13 -0400

    Reservation and Waiting List reservations

---
+++ js/tables.js
@@ -0,0 +1,13 @@
+// Array for Table Reservation
+var tableArray = [
+  {
+    name: "John Doe",
+    phoneNumber: "000-000-0000"
+    email: "johndoe@example.com",
+    uniqueID: "johnDoe",
+
+  }
+];
+
+// Module Export for Table Reservation array
+module.exports = tableArray;
